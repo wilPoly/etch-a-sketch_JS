d498edf5c8cf214caf6e86517ae2aad91d796eaf
Capestan Guillaume <comcape@mairie-marlyleroi.fr>
2019-02-06 11:12:48 +0100

    Created index.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.getElementById('container');
+
+// On crée les divs (16X16)
+for (let i=1; i <= 16; i++) {
+  let div = document.createElement("div");
+  container.appendChild(div);
+}
