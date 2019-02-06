afb3466bc051b5ef53085f7105c20e55294c9ec8
Capestan Guillaume <comcape@mairie-marlyleroi.fr>
2019-02-06 12:09:11 +0100

    Lunch commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let container = document.getElementById('container');
-
+let size = prompt("Quelle taille de côté ?");
+let gridSize = size ** 2;
 // On crée les divs (16X16)
-for (let i=1; i <= 16; i++) {
+for (let i=0; i < gridSize ; i++) {
   let div = document.createElement("div");
+  div.setAttribute("class", "gridElement");
   container.appendChild(div);
 }
